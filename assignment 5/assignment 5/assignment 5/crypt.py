abf05528b81077cd920679c9fe9ca09d530271ba
mrmegaremote <remote.tv.kottelaar@gmail.com>
2015-09-30 13:14:45 +0200

    added comments

--- assignment 5/assignment 5/assignment 5/crypt.py
+++ assignment 5/assignment 5/assignment 5/crypt.py
@@ -1,18 +1,20 @@
 while True:
     #97 ~ 122
+    #definitions
     inp = raw_input("enter a phrase: \n")
     key = input("enter a key: \n")
 
     cryp = ""
     upper = bool
 
+    #encrypting the letter
     def crypt(a, b):
         new = ord(b) + a
         while new > 122:
             new -= 26
         return new
 
-
+    #finding wether the character should be encrypted
     for i in range (0, len(inp)):
         if inp[i].isalpha():
             if inp[i].isupper():
@@ -26,4 +28,6 @@ while True:
                 cryp += str.lower(chr(crypt(key, inp[i])))
         else:
             cryp += inp[i]
+
+    #printing the encrypted password
     print cryp
