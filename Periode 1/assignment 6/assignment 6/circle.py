5b28d509aa81210508599f912a8fda6d857fa71d
mrmegaremote <remote.tv.kottelaar@gmail.com>
2016-04-29 10:54:17 +0200

    launch file edited

--- Periode 1/assignment 6/assignment 6/circle.py
+++ Periode 1/assignment 6/assignment 6/circle.py
@@ -7,12 +7,12 @@ while True:
     for i in range(0, inp * 2):
         for j in range (0, inp * 2):
             di = (i - inp) ** 2
-            dj = (j - inp) ** 2 
+            dj = (j - inp) ** 2
             dist = sqrt(di + dj)
-            if inp > dist: 
+            if inp > dist:
                 cir += "*"
             else:
                 cir += " "
-            
+
         cir += "\n"
     print cir
